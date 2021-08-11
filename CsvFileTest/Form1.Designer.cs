d3cdb9434bd819e39c3e57a5143c24ae306cca64
hmji <hmji@nansoft.co.kr>
2021-08-11 16:09:23 +0900

    Add project files.

---
+++ CsvFileTest/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace CsvFileTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnWrite = new System.Windows.Forms.Button();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnWrite
+            // 
+            this.btnWrite.Location = new System.Drawing.Point(43, 26);
+            this.btnWrite.Name = "btnWrite";
+            this.btnWrite.Size = new System.Drawing.Size(108, 75);
+            this.btnWrite.TabIndex = 0;
+            this.btnWrite.Text = "Write";
+            this.btnWrite.UseVisualStyleBackColor = true;
+            this.btnWrite.Click += new System.EventHandler(this.btnWrite_Click);
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(194, 26);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(110, 75);
+            this.btnRead.TabIndex = 1;
+            this.btnRead.Text = "Read";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.btnWrite);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnWrite;
+        private System.Windows.Forms.Button btnRead;
+    }
+}
+
---
+++ CsvFileTest/Form1.cs
@@ -0,0 +1,74 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CsvFileTest
+{
+    //https://joshclose.github.io/CsvHelper/examples/csvdatareader/
+    //https://github.com/JoshClose/CsvHelper/issues/1726
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        CsvConfiguration config = new CsvConfiguration(CultureInfo.InvariantCulture)
+        {
+            HasHeaderRecord = false,
+            Delimiter = ";",
+            ShouldQuote = args => true
+        };
+
+        public class Foo
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            using (var reader = new StreamReader("file.csv"))
+            using (var csv = new CsvReader(reader, config))
+            {
+                var record = new Foo();
+                var records = csv.EnumerateRecords(record);
+                foreach (var r in records)
+                {
+                    Console.WriteLine(string.Format("{0} {1}", r.Id, r.Name));
+                }
+            }
+        }
+
+        private void btnWrite_Click(object sender, EventArgs e)
+        {
+            var records = new List<Foo>
+            {
+                new Foo { Id = 1, Name = "one" },
+                new Foo { Id = 2, Name = "two" },
+            };
+
+            using (var writer = new StreamWriter("file.csv"))
+            using (var csv = new CsvWriter(writer, config))
+            {
+                csv.WriteRecords(records);
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
